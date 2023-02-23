4406d1a933580f36417a53286a9484e64b41d09c
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2023-02-23 18:52:22 +0000

    Category work in progress.

--- classes/admin_setting_categoryconfigmultiselect.php
+++ classes/admin_setting_categoryconfigmultiselect.php
@@ -27,6 +27,8 @@
 
 namespace block_coursefilesarchive;
 
+require_once($CFG->libdir.'/blocklib.php');
+
 /**
  * Category config multi select setting that adjusts the shown blocks when the selected categories change.
  *
@@ -44,16 +46,46 @@ class admin_setting_categoryconfigmultiselect extends \admin_setting_configmulti
     public function post_write_settings($originalcats) {
         $parent = parent::post_write_settings($originalcats);
         if ($parent) {
-            // Incomplete.
             $newvalues = $this->get_setting();
 
-            $removedcats = array();
+            $removedcats = array(); // Array of removed category id's from the setting.
             foreach($originalcats as $originalcat) {
                 if (!in_array($originalcat, $newvalues)) {
                     $removedcats[] = $originalcat;
                 }
             }
-
+            if (!empty($removedcats)) {
+                /*
+                    The block has a parent context, for this block, that will be the course.  The parent context
+                    instance id is the course id.  If we get all of the courses for the removed category, we can
+                    get their course id's, then get their context ids.  Then with a list of our block instances,
+                    we can see if the course context is in our list of mapped parent id's to block instance id's.
+                    If it is then we know that block needs to be deleted.  We do this by adding the id to list of
+                    id's to delete, then call the appropriate blocklib function.
+                */
+                global $DB;
+                $blockinstances = $DB->get_recordset('block_instances', array('blockname' => 'coursefilesarchive'));
+                if (!empty($blockinstances)) {
+                    $blockinstancesparentcontext = array();
+                    foreach($blockinstances as $blockinstance) {
+                        $blockinstancesparentcontext[$blockinstance->parentcontextid] = $blockinstance->id;
+                    }
+                    $blockstodelete = array();
+                    foreach($removedcats as $removedcat) {
+                        $category = \core_course_category::get($removedcat);
+                        $catcourses = $category->get_courses();
+                        foreach($catcourses as $catcourse) {
+                            $catcoursescontextid = $catcourse->get_context()->id;
+                            if (array_key_exists($catcoursescontextid, $blockinstancesparentcontext)) {
+                                $blockstodelete[] = $blockinstancesparentcontext[$catcoursescontextid];
+                            }
+                        }
+                    }
+                    if (!empty($blockstodelete)) {
+                        blocks_delete_instances($blockstodelete);
+                    }
+                }
+            }
         } else {
             return false;
         }
